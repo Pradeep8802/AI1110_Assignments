d8daa3fb7aa13113cb3cc12b554c655acc6ba420
Pradeep8802 <102598014+Pradeep8802@users.noreply.github.com>
2022-05-17 09:42:03 +0530

    Update pythoncode.py

--- Assignment3/code/pythoncode.py
+++ Assignment3/code/pythoncode.py
@@ -9,7 +9,7 @@ Question 8
 '''
 """
 Question Statement:
-A factory has two machines A and B. Past record shows that machine A produced 60\% of th items of output and machine B produced 40\% of the items. Further,2\% of the items produced by machine A and 1\% produced by machine B were defective. All the items are put into one stockpile and then one item is chosen at random from this and is found to be defective. What is the probability that it was produced by machine B?
+A factory has two machines A and B. Past record shows that machine A produced 60% of th items of output and machine B produced 40% of the items. Further,2% of the items produced by machine A and 1% produced by machine B were defective. All the items are put into one stockpile and then one item is chosen at random from this and is found to be defective. What is the probability that it was produced by machine B?
 """
 
 import numpy as np
